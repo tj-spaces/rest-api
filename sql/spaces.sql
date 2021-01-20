1294f8ae925469a90f579231b50232befee5e81c
Michael Fatemi <myfatemi04@gmail.com>
2021-01-19 23:15:01 -0500

    Update spaces.sql

--- sql/spaces.sql
+++ sql/spaces.sql
@@ -4,23 +4,25 @@ CREATE TABLE `spaces` (
 	`id` VARCHAR(36),
 	`name` VARCHAR(256),
 	`color` VARCHAR(32),
+
+	-- For now, only 'cluster'
 	`type` ENUM('group', 'cluster'),
 
 	-- Can be NULL
 	`cluster_id` VARCHAR(36),
 
 	-- Can be NULL
-	`group_id` VARCHAR(36),
+	/* `group_id` VARCHAR(36), */
 
 	CONSTRAINT `fk_space__cluster`
 		FOREIGN KEY (`cluster_id`)
 			REFERENCES `clusters`(`id`)
 				ON DELETE CASCADE,
 
-	CONSTRAINT `fk_space__group`
+	/* CONSTRAINT `fk_space__group`
 		FOREIGN KEY (`group_id`)
 			REFERENCES `groups`(`id`)
-				ON DELETE CASCADE,
+				ON DELETE CASCADE, */
 
 	PRIMARY KEY (`id`)
 );
