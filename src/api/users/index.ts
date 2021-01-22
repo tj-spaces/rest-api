5bf14c4775f2836c90a831431b43256c29a9df96
Michael Fatemi <myfatemi04@gmail.com>
2021-01-21 21:42:58 -0500

    Add /api/users/@me

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { getClustersWithUser } from "../../database/tables/cluster_members";
+import { getUserFromId } from "../../database/tables/users";
 import requireApiAuth from "../../middleware/requireApiAuth";
 
 export const router = Router();
+
+router.get("/@me", requireApiAuth, async (req, res) => {
+  const { accountId } = req.session;
+  const user = await getUserFromId(accountId);
+  res.json({ status: "success", user });
+});
 
 router.get("/@me/clusters", requireApiAuth, async (req, res) => {
   const { accountId } = req.session;
