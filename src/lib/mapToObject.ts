5b091c0f216d5433e6bba8121a957cf02af63a62
Michael Fatemi <myfatemi04@gmail.com>
2021-01-22 17:09:49 -0500

    remove advanced compileroptions

--- src/lib/mapToObject.ts
+++ src/lib/mapToObject.ts
@@ -1,6 +1,6 @@
 export default function mapToObject<V>(map: Map<string | number, V>) {
   let obj: { [key: string]: V } = {};
-  for (let [key, value] of map.entries()) {
+  for (let [key, value] of Array.from(map.entries())) {
     obj[key] = value;
   }
 
