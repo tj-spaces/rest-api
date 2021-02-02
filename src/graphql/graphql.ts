b5262dcbaa757ecdc30aae27ee7c311d61c57713
Michael Fatemi <myfatemi04@gmail.com>
2021-02-01 22:01:01 -0500

    Add mergeResolverSchemas.ts

--- src/graphql/graphql.ts
+++ src/graphql/graphql.ts
@@ -1,8 +1,7 @@
-import { gql, makeExecutableSchema } from "apollo-server-express";
-
+import { makeExecutableSchema } from "apollo-server-express";
 import * as cluster from "./cluster";
-import * as inviteLink from "./inviteLink";
 import * as group from "./group";
+import * as inviteLink from "./inviteLink";
 import * as space from "./space";
 import * as user from "./user";
 
