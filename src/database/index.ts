2bade1fb0983b34b3ebf11eaafb56a6cca24c0d8
Michael Fatemi <myfatemi04@gmail.com>
2021-03-02 13:15:56 -0500

    Add message for when db connection is established

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,14 @@
-import { Client, Pool } from "pg";
+import { Pool } from "pg";
 
 function getDatabaseUrl() {
   return process.env.DATABASE_URL ?? process.env.DIRECTOR_DATABASE_URL;
 }
 
 export const db = new Pool({ connectionString: getDatabaseUrl() });
-db.connect();
+db.connect()
+  .then(() => {
+    console.log("Connected to Postgres");
+  })
+  .catch((err) => {
+    console.error("Error connecting to Postgres:", err);
+  });
