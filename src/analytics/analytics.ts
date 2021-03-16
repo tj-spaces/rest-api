346da5f061694ae710723280384a1a7a04103078
Michael Fatemi <myfatemi04@gmail.com>
2021-03-15 21:32:56 -0400

    Add res.end()s

--- src/analytics/analytics.ts
+++ src/analytics/analytics.ts
@@ -7,8 +7,10 @@ router.post("/error", (req, res) => {
   if (!("error" in body)) {
     console.warn("Received malformed Error log:", body);
     res.status(400);
+    res.end();
   } else {
     console.error("CLIENT ERROR:", body.error);
     res.status(200);
+    res.end();
   }
 });
